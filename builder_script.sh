2c2b9917338f814b2c01b12aa20ffe80134bf78f
electronicslover <luizpaulodamaceno@live.com>
2017-02-10 12:17:30 -0200

    Update builder_script.sh

--- builder_script.sh
+++ builder_script.sh
@@ -1,7 +1,7 @@
 repo sync;
 export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -Xmx7g";
 export WITH_SU=true ;
-./prebuilts/sdk/tools/jack-admin stop-server
+./prebuilts/sdk/tools/jack-admin stop-server ;
 ./prebuilts/sdk/tools/jack-admin start-server;
 . build/envsetup.sh ;
 breakfast A6020;
