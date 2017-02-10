de5b339ba9ef6a228d255dfb7a9a20d8b5004348
electronicslover <luizpaulodamaceno@live.com>
2017-02-10 12:16:15 -0200

    Adding envsetup command, i forgot it.

--- builder_script.sh
+++ builder_script.sh
@@ -3,6 +3,7 @@ export JACK_SERVER_VM_ARGUMENTS="-Dfile.encoding=UTF-8 -XX:+TieredCompilation -X
 export WITH_SU=true ;
 ./prebuilts/sdk/tools/jack-admin stop-server
 ./prebuilts/sdk/tools/jack-admin start-server;
+. build/envsetup.sh
 breakfast A6020;
 brunch A6020;
 ./prebuilts/sdk/tools/jack-admin stop-server;
